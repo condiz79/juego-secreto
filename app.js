38f262f999185f978542585529bd386c5bf08337
condiz79 <49282922+condiz79@users.noreply.github.com>
2025-01-20 18:25:42 -0300

    Versión final juego número secreto

---
+++ app.js
@@ -0,0 +1,38 @@
+//Variables
+let numeroMaximoPosible = 100;
+let numeroSecreto = Math.floor(Math.random()*numeroMaximoPosible)+1;
+let numeroUsuario = 0;
+let intentos = 1;
+//let palabraVeces = 'vez';
+let maximosIntentos = 6;
+while (numeroUsuario != numeroSecreto) {
+    numeroUsuario = parseInt(prompt(`Me indicas un número ente 1 y ${numeroMaximoPosible} por favor:`));
+
+    console.log(numeroUsuario);
+    /*
+    Este código realiza
+    la comparación
+    */
+    if (numeroUsuario == numeroSecreto) {
+        //Acertamos, fue verdadera la condición
+        alert(`Acertaste, el número es: ${numeroUsuario}. Lo hiciste en ${intentos} ${intentos == 1 ? 'vez' : 'veces'}`);
+    }
+    else{
+        if (numeroUsuario > numeroSecreto){
+            alert('El número secreto es menor');
+        }
+        else {
+            alert('El número secreto es mayor');
+        }
+        //Incrementamos el contador cuando no acierte
+        intentos++;
+        //palabraVeces = 'veces';
+        if (intentos > maximosIntentos) {
+            alert(`Llegaste al número máximo de ${maximosIntentos} intentos`);
+            break;
+        }
+        //La condición no se cumplió
+        //alert('Lo siento, no acertaste el número');
+    }
+}
+
